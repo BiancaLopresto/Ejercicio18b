d3711d8d2c7b925ad30035cf83ba7493908e815b
BiancaLopresto <bianlopre19@gmail.com>
2023-05-21 21:35:35 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio18b.Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Timers;
+
+namespace Ejercicio18b.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Grado;
+            double Multiplicacion;
+
+            Console.Write("Ingrese el valor del grado: ");
+            Grado= int.Parse(Console.ReadLine());
+            
+            Multiplicacion = Grado * 0.0174532925;
+
+            if (Grado > 0)
+            {
+                Console.Write($"El grado pasado a radian es {Multiplicacion}");
+            }else
+            {
+                Console.Write("El Grado debe ser positivo");
+            }
+        }
+    }
+}
